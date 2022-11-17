97744b3a900d006df8f1aef7b56076a21f863aca
seasontemple <fnc1997@foxmail.com>
2022-11-17 14:02:33 +0800

    2nd

--- src/main/java/com/st/srcreview/SrcReviewApplication.java
+++ src/main/java/com/st/srcreview/SrcReviewApplication.java
@@ -13,6 +13,7 @@ import org.springframework.context.annotation.Bean;
 public class SrcReviewApplication {
 
     private final int a = 1;
+    private final int b = 1;
 
     @Bean
     public A A() {
